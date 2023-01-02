daef45d669b33af155570d46ae8181363e96229d
mohanaddevops <hanatareka@gmail.com>
2023-01-02 09:37:35 +0200

    set env

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=mohanad
 export POSTGRES_PASSWORD=123456789
-export POSTGRES_HOST=cdr.csugw3smm9dl.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cgoxjcexskjj.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udagram-project-udacity
+export AWS_BUCKET=arn:aws:s3:::udacity-gram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=hello
+export JWT_SECRET=helloworld
 export URL=http://localhost:8100
